6bf48c970a3fcf82570bb502a411fad781882c51
Michael Stumme <mstumme@mines.edu>
2021-09-30 16:02:14 -0600

    Created the TestBoardCell class

---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,37 @@
+package experiment;
+
+import java.util.Set;
+
+public class TestBoardCell {
+
+	public TestBoardCell(int row, int col) {
+		super();
+	}
+	
+	public void addAdjacency(TestBoardCell cell) {
+		
+	}
+	
+	public Set<TestBoardCell> getAdjList(){
+		Set<TestBoardCell> cells = null;
+		return cells;
+		
+	}
+	
+	public void setRoom(boolean room) {
+		
+	}
+	
+	public boolean isRoom() {
+		return false;
+	}
+	
+	public void setOccupied(boolean occupied) {
+		
+	}
+	
+	public boolean getOccupied() {
+		return false;
+	}
+
+}
