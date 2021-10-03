0ea352a95c512d9e3b76642d31fad94fb1be0aa3
Michael Stumme <mstumme@mines.edu>
2021-10-03 09:57:54 -0600

    Created the tests for the targets

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,7 +1,5 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Set;
 
 import org.junit.Assert;
@@ -57,5 +55,97 @@ class BoardTestsExp {
 		Assert.assertTrue(testListMiddleGrid.contains(board.getCell(2, 3)));
 		Assert.assertTrue(testListMiddleGrid.contains(board.getCell(3, 2)));
 	}
+	
+	// testTargetCreation - used to test targets on an empty board
+	@Test
+	public void testTargetEmpty() {
+		
+		//Test an empty board
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell,3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+		
+	}
+	
+	// testTargetOccupied - used to test targets on occupied board
+	@Test
+	public void testTargetOccupied() {
+		
+		// Tests if targets is set correctly with occupied board
+		board.getCell(0,1).setOccupied(true);
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell,2);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(2, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 1)));
+	
+	}
+	
+	// testTargetRoom - used to test targets on board with room in it
+	@Test
+	public void testTargetRoom() {
+		
+		// Tests if targets is set correctly with a board with a room in it
+		board.getCell(1,1).setRoom(true);
+		TestBoardCell cell = board.getCell(2, 1);
+		board.calcTargets(cell,3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(8, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
+	
+	}
+	
+	// testTargetMixed1- used to test targets on board with room and occupied
+	@Test
+	public void testTargetMixed1() {
+		
+		// Tests if targets is set correctly with occupied board and a room
+		board.getCell(0,2).setOccupied(true);
+		board.getCell(1,2).setRoom(true);
+		TestBoardCell cell = board.getCell(0, 3);
+		board.calcTargets(cell,4);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 2)));
+	
+	}
+		
+	// testTargetMixed2- used to test targets on board with room and occupied
+	@Test
+	public void testTargetMixed2() {
+		
+		// Tests if targets is set correctly with occupied board and a room
+		board.getCell(1,1).setOccupied(true);
+		board.getCell(2,2).setRoom(true);
+		TestBoardCell cell = board.getCell(2, 1);
+		board.calcTargets(cell,5);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(5, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
+	
+	}
+	
+	
 
 }
