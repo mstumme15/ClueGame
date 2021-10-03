1e4b79693328fff8dbc5dfd849d01234f92cb8dc
Liam Wedell <lwedell@mines.edu>
2021-10-02 21:00:52 -0600

    Add Comments to TestBoardCell and TestBoard

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -5,18 +5,26 @@ import java.util.Set;
 
 public class TestBoard {
 
+	// Default constructor - sets up the board
+	// TODO: implement
 	public TestBoard() {
 		super();
 	}
 	
+	// calcTargets - calculates legal move targets given the starting cell and the path length
+	// TODO: implement
 	public void calcTargets(TestBoardCell startCell, int pathLength) {
 		
 	}
 	
+	// getTargets - getter for values calculated in calcTargets
+	// TODO: implement
 	public Set<TestBoardCell> getTargets() {
 		return new HashSet<TestBoardCell>();
 	}
 	
+	// getCell - getter that returns the cell at location of row, column
+	// TODO: implement
 	public TestBoardCell getCell(int row, int column) {
 		return new TestBoardCell(0, 0);
 	}
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -5,31 +5,41 @@ import java.util.Set;
 
 public class TestBoardCell {
 
+	// Parameterized constructor - sets up cell at location row, column
+	// TODO: implement
 	public TestBoardCell(int row, int col) {
 		super();
 	}
 	
+	// addAdjacency - setter that adds a cell to the adjacency list
+	// TODO: implement
 	public void addAdjacency(TestBoardCell cell) {
 		
 	}
 	
+	// TODO: implement
 	public Set<TestBoardCell> getAdjList(){
 		return new HashSet<TestBoardCell>();
 		
 	}
 	
+	// TODO: implement
 	public void setRoom(boolean room) {
 		
 	}
 	
+	// Checks to see if the cell is in a room
+	// TODO: implement
 	public boolean isRoom() {
 		return false;
 	}
 	
+	// TODO: implement
 	public void setOccupied(boolean occupied) {
 		
 	}
 	
+	// TODO: implement
 	public boolean getOccupied() {
 		return false;
 	}
