cd6318d7408acc02727652d1f5b36e371dde18de
Liam Wedell <lwedell@mines.edu>
2021-10-07 11:28:02 -0600

    Created class stubs in clueGame package

---
+++ src/clueGame/Board.java
@@ -0,0 +1,24 @@
+package clueGame;
+
+public class Board {
+	
+	private Board() {
+		
+	}
+	
+	public static Board getInstance() {
+		return new Board();
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public void loadSetupConfig() {
+		
+	}
+	
+	public void loadLayoutConfig() {
+		
+	}
+}
---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,12 @@
+package clueGame;
+
+public class BoardCell {
+
+	public BoardCell() {
+		
+	}
+	
+	public void addAdj(BoardCell adj) {
+		
+	}
+}
